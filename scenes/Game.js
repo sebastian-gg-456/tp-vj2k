8a44b173da64498b4c8d72c882746e233cae8eb9
sebastian gonzales garcia <gonzalesgarcia811@gmail.com>
2025-04-29 19:26:00 -0300

    Initialize feature/first-game-p3

---
+++ scenes/Game.js
@@ -0,0 +1,37 @@
+// URL to explain PHASER scene: https://rexrainbow.github.io/phaser3-rex-notes/docs/site/scene/
+
+export default class Game extends Phaser.Scene {
+  constructor() {
+    // key of the scene
+    // the key will be used to start the scene by other scenes
+    super("game");
+  }
+
+  init() {
+    // this is called before the scene is created
+    // init variables
+    // take data passed from other scenes
+    // data object param {}
+  }
+
+  preload() {
+    // load assets
+    this.load.image("sky", "./public/assets/sky.png");
+    this.load.image("ground", "./public/assets/platform.png");
+    this.load.image("star", "./public/assets/star.png");
+    this.load.image("bomb", "./public/assets/bomb.png");
+    this.load.spritesheet("dude", "./public/assets/dude.png", {
+      frameWidth: 32,
+      frameHeight: 48,
+    });
+  }
+
+  create() {
+    // create game objects
+    this.add.image(400, 300, "sky");
+  }
+
+  update() {
+    // update game objects
+  }
+}
