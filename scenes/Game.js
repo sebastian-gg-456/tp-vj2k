ccfde3fc7e8cc4e9a3a26d120cbcf90a909de591
sebastian gonzales garcia <gonzalesgarcia811@gmail.com>
2025-04-29 19:26:02 -0300

    Initialize feature/first-game-p7

---
+++ scenes/Game.js
@@ -0,0 +1,91 @@
+// URL to explain PHASER scene: https://rexrainbow.github.io/phaser3-rex-notes/docs/site/scene/
+
+export default class Game extends Phaser.Scene {
+  constructor() {
+    // key of the scene
+    // the key will be used to start the scene by other scenes
+    super("game");
+  }
+
+  init() {
+    // this is called before the scene is created
+    // init variables
+    // take data passed from other scenes
+    // data object param {}
+  }
+
+  preload() {
+    // load assets
+    this.load.image("sky", "./public/assets/sky.png");
+    this.load.image("ground", "./public/assets/platform.png");
+    this.load.image("star", "./public/assets/star.png");
+    this.load.image("bomb", "./public/assets/bomb.png");
+    this.load.spritesheet("dude", "./public/assets/dude.png", {
+      frameWidth: 32,
+      frameHeight: 48,
+    });
+  }
+
+  create() {
+    // create game objects
+    this.add.image(400, 300, "sky");
+
+    this.platforms = this.physics.add.staticGroup();
+
+    this.platforms.create(400, 568, "ground").setScale(2).refreshBody();
+
+    this.platforms.create(600, 400, "ground");
+    this.platforms.create(50, 250, "ground");
+    this.platforms.create(750, 220, "ground");
+
+    this.player = this.physics.add.sprite(100, 450, "dude");
+
+    this.player.setBounce(0.2);
+    this.player.setCollideWorldBounds(true);
+
+    this.anims.create({
+      key: "left",
+      frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "turn",
+      frames: [{ key: "dude", frame: 4 }],
+      frameRate: 20,
+    });
+
+    this.anims.create({
+      key: "right",
+      frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.physics.add.collider(this.player, this.platforms);
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  update() {
+    // update game objects
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-160);
+
+      this.player.anims.play("left", true);
+    } else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(160);
+
+      this.player.anims.play("right", true);
+    } else {
+      this.player.setVelocityX(0);
+
+      this.player.anims.play("turn");
+    }
+
+    if (this.cursors.up.isDown && this.player.body.touching.down) {
+      this.player.setVelocityY(-330);
+    }
+  }
+}
